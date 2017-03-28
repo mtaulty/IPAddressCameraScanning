2ce56b2d5c3a33fc65a86da9e0f991e18c110462
Mike Taulty <mtaulty@microsoft.com>
2017-03-28 21:17:03 +0100

    fixing build problems

---
+++ UnityProject/Assets/Placeholder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Placeholder : MonoBehaviour
+{
+  public Transform textMeshObject;
+
+  private void Start()
+  {
+    this.textMesh = this.textMeshObject.GetComponent<TextMesh>();
+    this.OnReset();
+  }
+  public void OnScan()
+  {
+    this.textMesh.text = "scanning for 30s";
+
+#if !UNITY_EDITOR
+    MediaFrameQrProcessing.Wrappers.IPAddressScanner.ScanFirstCameraForIPAddress(
+        result =>
+        {
+          UnityEngine.WSA.Application.InvokeOnAppThread(() =>
+          {
+            // result here is a System.Net.IPAddress...
+            this.textMesh.text = result?.ToString() ?? "not found";
+          }, 
+          false);
+        },
+        TimeSpan.FromSeconds(30));
+#endif
+  }
+  public void OnReset()
+  {
+    this.textMesh.text = "say scan to start";
+  }
+  TextMesh textMesh;
+}
