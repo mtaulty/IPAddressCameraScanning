6b838880bea59501062f0d9a19f0264d2c83bc8c
Mike Taulty <mtaulty@microsoft.com>
2017-03-28 14:37:47 +0100

    Initial commit for blog post

---
+++ MediaFrameQrProcessing/Processors/IPAddressFrameProcessor.cs
@@ -0,0 +1,78 @@
+﻿namespace MediaFrameQrProcessing.Processors
+{
+  using MediaFrameQrProcessing.VideoDeviceFinders;
+  using System.Runtime.InteropServices.WindowsRuntime;
+  using System.Threading.Tasks;
+  using Windows.Devices.Enumeration;
+  using Windows.Media.Capture;
+  using Windows.Media.Capture.Frames;
+  using Windows.Media.Ocr;
+  using System;
+  using System.Text.RegularExpressions;
+  using System.Net;
+
+  public class IPAddressFrameProcessor : MediaCaptureFrameProcessor
+  {
+    public IPAddress Result { get; private set; }
+
+    public IPAddressFrameProcessor(
+      MediaFrameSourceFinder mediaFrameSourceFinder, 
+      DeviceInformation videoDeviceInformation, 
+      string mediaEncodingSubtype, 
+      MediaCaptureMemoryPreference memoryPreference = MediaCaptureMemoryPreference.Cpu) 
+
+      : base(
+          mediaFrameSourceFinder, 
+          videoDeviceInformation, 
+          mediaEncodingSubtype, 
+          memoryPreference)
+    {
+    }
+    protected override async Task<bool> ProcessFrameAsync(MediaFrameReference frameReference)
+    {
+      bool done = false;
+
+      // doc here https://msdn.microsoft.com/en-us/library/windows/apps/xaml/windows.media.capture.frames.videomediaframe.aspx
+      // says to dispose this softwarebitmap if you access it.
+      using (var bitmap = frameReference.VideoMediaFrame.SoftwareBitmap)
+      {
+        try
+        {
+          if (this.ocrEngine == null)
+          {
+            this.ocrEngine = OcrEngine.TryCreateFromUserProfileLanguages();
+            this.regex = new Regex(IP_ADDRESS_PATTERN);
+          }
+          var results = await this.ocrEngine.RecognizeAsync(bitmap);
+
+          if (results != null)
+          {
+            var matchingResults = this.regex.Matches(results.Text);
+
+            for (int i = 0; !done && (i < matchingResults.Count); i++)
+            {
+              IPAddress parsedAddress;
+
+              done = IPAddress.TryParse(matchingResults[i].Value, out parsedAddress);
+
+              if (done)
+              {
+                this.Result = parsedAddress;
+              }
+            }
+          }
+        }
+        catch
+        {
+        }
+      }
+      return (done);
+    }
+    Regex regex;
+    OcrEngine ocrEngine;
+
+    // Taken from S.O. http://stackoverflow.com/questions/106179/regular-expression-to-match-dns-hostname-or-ip-address
+    const string IP_ADDRESS_PATTERN =
+      @"(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}";
+  }
+}
---
+++ MediaFrameQrProcessing/Wrappers/IPAddressScanner.cs
@@ -0,0 +1,66 @@
+﻿namespace MediaFrameQrProcessing.Wrappers
+{
+  using MediaFrameQrProcessing.Processors;
+  using MediaFrameQrProcessing.VideoDeviceFinders;
+  using System;
+  using System.Net;
+  using Windows.Media.MediaProperties;
+
+  public static class IPAddressScanner
+  {
+    public static async void ScanFirstCameraForIPAddress(
+      Action<IPAddress> resultCallback,
+      TimeSpan timeout)
+    {
+      IPAddress result = null;
+
+      // Note - I keep this frame processor around which means keeping the
+      // underlying MediaCapture around because when I didn't keep it
+      // around I ended up with a crash in Windows.Media.dll related
+      // to disposing of the MediaCapture.
+      // So...this isn't what I wanted but it seems to work better :-(
+      if (frameProcessor == null)
+      {
+        var mediaFrameSourceFinder = new MediaFrameSourceFinder();
+
+        // We want a source of media frame groups which contains a color video
+        // preview (and we'll take the first one).
+        var populated = await mediaFrameSourceFinder.PopulateAsync(
+          MediaFrameSourceFinder.ColorVideoPreviewFilter,
+          MediaFrameSourceFinder.FirstOrDefault);
+
+        if (populated)
+        {
+          // We'll take the first video capture device.
+          var videoCaptureDevice =
+            await VideoCaptureDeviceFinder.FindFirstOrDefaultAsync();
+
+          if (videoCaptureDevice != null)
+          {
+            // Make a processor which will pull frames from the camera and run
+            // ZXing over them to look for QR codes.
+            frameProcessor = new IPAddressFrameProcessor(
+              mediaFrameSourceFinder,
+              videoCaptureDevice,
+              MediaEncodingSubtypes.Bgra8);
+
+            // Remember to ask for auto-focus on the video capture device.
+            frameProcessor.SetVideoDeviceControllerInitialiser(
+              vd => vd.Focus.TrySetAuto(true));
+          }
+        }
+      }
+      if (frameProcessor != null)
+      {
+        // Process frames for up to 30 seconds to see if we get any QR codes...
+        await frameProcessor.ProcessFramesAsync(timeout);
+
+        // See what result we got.
+        result = frameProcessor.Result;
+      }
+      // Call back with whatever result we got.
+      resultCallback(result);
+    }
+    static IPAddressFrameProcessor frameProcessor;
+  }
+}
